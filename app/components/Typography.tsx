3653943d6ca26d1051b366f12762af9d4fcc7da3
Lorenzo Bertolini <lorenzo@vento.io>
2022-05-13 16:19:42 +0200

    send over minimum data structure

--- app/components/Typography.tsx
+++ app/components/Typography.tsx
@@ -2,8 +2,16 @@ import { ReactNode } from "react";
 
 export function Heading1({ children }: { children: ReactNode }) {
   return (
-    <h1 className="font-heading my-0 text-3xl leading-8 tracking-wide text-neue-header dark:text-neuedark-header lg:text-4xl">
+    <h1 className="my-0 font-heading text-3xl leading-8 tracking-wide text-neue-header dark:text-neuedark-header lg:text-4xl">
       {children}
     </h1>
+  );
+}
+
+export function Heading2({ children }: { children: ReactNode }) {
+  return (
+    <h2 className="mt-0 mb-4 font-heading text-2xl leading-8 tracking-wide text-neue-header dark:text-neuedark-header lg:text-2xl">
+      {children}
+    </h2>
   );
 }
--- app/routes/portal/index.tsx
+++ app/routes/portal/index.tsx
@@ -1,9 +1,62 @@
-import { Heading1 } from "~/components/Typography";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { Outlet, Link, Form, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+
+import { Heading1, Heading2 } from "~/components/Typography";
+
+type PerformanceOverviewData = {
+  data: Array<{ date: Date; value: number }>;
+};
+
+type LoaderData = {
+  // user: Awaited<ReturnType<typeof getUser>>;
+  // jokeListItems: Array<{ id: string; name: string }>;
+  portfolios: Array<{ name: string }>;
+  performance: {
+    "1M": PerformanceOverviewData;
+    YTD: PerformanceOverviewData;
+  };
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  // const jokeListItems = await db.joke.findMany({
+  //   take: 5,
+  //   select: { id: true, name: true },
+  //   orderBy: { createdAt: "desc" },
+  // });
+
+  // const user = await getUser(request);
+
+  const data: LoaderData = {
+    // jokeListItems,
+    portfolios: [
+      { name: "G10 Momentum" },
+      { name: "DeFi" },
+      { name: "Bitcoin" },
+    ],
+    performance: {
+      "1M": {
+        data: [{ date: new Date(), value: 100 }],
+      },
+      YTD: {
+        data: [{ date: new Date(), value: 100 }],
+      },
+    },
+  };
+
+  return json(data);
+};
 
 export default function PortalIndexPage() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div>
       <Heading1>Portfolio</Heading1>
+      <Heading2>Your Performance</Heading2>
+      <div>{JSON.stringify(data.performance, undefined, 2)}</div>
+      <Heading2>Your Portfolios</Heading2>
+      <div>{JSON.stringify(data.portfolios, undefined, 2)}</div>
     </div>
   );
 }
