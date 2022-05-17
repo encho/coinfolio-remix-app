cbd5e863e36fa1f534cfdacd631c0b987365f1d7
Lorenzo Bertolini <lorenzo@vento.io>
2022-05-17 11:22:56 +0200

    integrate sparkline chart in strategies page

--- app/routes/portal/strategies/index.tsx
+++ app/routes/portal/strategies/index.tsx
@@ -1,5 +1,16 @@
 import { Link } from "@remix-run/react";
 import { PageTitle, SectionTitle } from "~/components/Typography";
+import { SparklineChart } from "~/components/SparklineChart";
+
+const performanceSeries = [
+  { date: new Date("2022-01-01"), value: 100 },
+  { date: new Date("2022-01-02"), value: 110 },
+  { date: new Date("2022-01-03"), value: 105 },
+  { date: new Date("2022-01-04"), value: 120 },
+  { date: new Date("2022-01-05"), value: 110 },
+  { date: new Date("2022-01-06"), value: 130 },
+  { date: new Date("2022-01-07"), value: 120 },
+];
 
 export default function PortalStrategiesPage() {
   return (
@@ -14,31 +25,36 @@ export default function PortalStrategiesPage() {
                 name: "Bitcoin",
                 description:
                   "The worlds first digital currency. Bitcoin is tamperproof and openly traded.",
-                performanceSeries: [],
+                performanceSeries,
+                slug: "bitcoin",
               },
               {
                 name: "Ethereum",
                 description:
                   "Ethereum is a global virtual machine powered by blockchain technology.",
-                performanceSeries: [],
+                performanceSeries,
+                slug: "ethereum",
               },
               {
                 name: "Ripple",
                 description:
                   "Ripple is both a cryptocurrency and a digital payment network for financial transactions.",
-                performanceSeries: [],
+                performanceSeries,
+                slug: "ripple",
               },
               {
                 name: "Cardano",
                 description:
                   "Cardano is a blockchain and smart contracts platform with a cryptocurrency called ada.",
-                performanceSeries: [],
+                performanceSeries,
+                slug: "cardano",
               },
               {
                 name: "Solana",
                 description:
                   "Solana is a blockchain platform designed to host decentralized applications.",
-                performanceSeries: [],
+                performanceSeries,
+                slug: "solana",
               },
             ]}
           />
@@ -51,13 +67,15 @@ export default function PortalStrategiesPage() {
                 name: "G5 Equally Weighted Index",
                 description:
                   "Our equally weighted Cryptocurrency Index composed of the 5 largest cryptocurrencies.",
-                performanceSeries: [],
+                performanceSeries,
+                slug: "G5-equally-weighted",
               },
               {
                 name: "G5 Volatility Weighted Index",
                 description:
                   "Our volatility weighted Cryptocurrency Index composed of the 5 largest cryptocurrencies.",
-                performanceSeries: [],
+                performanceSeries,
+                slug: "G5-vola-weighted",
               },
             ]}
           />
@@ -72,6 +90,7 @@ type TStrategyOverview = {
   name: string;
   description: string;
   performanceSeries: Array<{ date: Date; value: number }>;
+  slug: string;
 };
 
 type TStrategyTilesProps = {
@@ -87,6 +106,7 @@ function StrategyTiles({ data }: TStrategyTilesProps) {
           name={strategy.name}
           description={strategy.description}
           performanceSeries={strategy.performanceSeries}
+          slug={strategy.slug}
         />
       ))}
     </div>
@@ -99,38 +119,28 @@ function StrategyTile({
   name,
   description,
   performanceSeries,
+  slug,
 }: TStrategyTileProps) {
   return (
     <div
       key={name}
       className="items-centerxxxx relative flex space-x-3 rounded border border-gray-200 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 hover:border-gray-350"
     >
-      {/* <div className="flex-shrink-0">
-        <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
-      </div> */}
       <div className="min-w-0 flex-1">
-        <Link
-          to={"./bitcoin"}
-          prefetch="intent"
-          //   className="hover:text-neue-link-hover-text text-neue-link-default-text"
-          className="focus:outline-none"
-        >
-          <div className="flex h-full flex-col justify-between">
-            <div className="mb-2">
+        <Link to={`./${slug}`} prefetch="intent" className="focus:outline-none">
+          <div className="flex h-full flex-col justify-between gap-4">
+            <div className="">
               <span className="absolute inset-0" aria-hidden="true" />
               <p className="text-sm font-bold text-gray-900">{name}</p>
-              {/* <p className="truncate text-sm text-gray-500">{description}</p> */}
-              {/* <p className="text-sm text-gray-500">{description}</p> */}
               <p className="text-sm text-gray-900">{description}</p>
             </div>
             <div>
-              <p className="h-20 w-full bg-gray-100"></p>
+              <div className="h-16 w-full">
+                <SparklineChart data={performanceSeries} />
+              </div>
             </div>
           </div>
         </Link>
-
-        {/* <a href="#" className="focus:outline-none">
-        </a> */}
       </div>
     </div>
   );
